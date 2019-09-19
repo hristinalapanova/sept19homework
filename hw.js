90afff0957957ba1d9c8b8bbd2c72fa930e81848
Hristina Lapanova <hristina.lapanova@gmail.com>
2019-09-19 16:42:44 -0400

    third problem

--- hw.js
+++ hw.js
@@ -9,3 +9,20 @@ Me["age"]   = 43;
 console.log(`${Me.age}`)
 Me["place of residence"]= "Burgas"
 console.log(Me["place of residence"])
+
+const monster = {
+    name: "Slimer",
+    color: "greenish",
+    type: "plasm or ghost or something",
+    introduce: function(){
+        console.log(`My name is ${this.name} and I am ${this.age} years old.`)
+    }
+
+}
+console.log(`${monster.name}`)
+monster["type"]="creature"
+console.log(monster)
+monster["age"]= 6
+console.log(monster)
+
+monster.introduce()
